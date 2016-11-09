d76764ffd7151df3bc56d4fa6ba173842662991c
Derek <derek@dinsmore-design.com>
2016-11-09 14:38:59 -0700

    Adding props & form

--- public/app.jsx
+++ public/app.jsx
@@ -1,15 +1,37 @@
 var Greeter = React.createClass({
+  getDefaultProps: function() {
+    return {
+      name: 'React',
+      message: 'This is a default message'
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    alert(name);
+  },
   render: function() {
+    var name = this.props.name;
+    var message = this.props.message
+
     return (
       <div>
-        <h1>Hello React!</h1>
-        <p>This is from a component</p>
+        <h1>Hello {name}!</h1>
+        <p>{message + '!!'}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
+
       </div>
     );
   }
 });
 
+var firstName = 'Derek';
+
 ReactDOM.render(
-  <Greeter/>,
+  <Greeter name={firstName}/>,
   document.getElementById('app')
 );
